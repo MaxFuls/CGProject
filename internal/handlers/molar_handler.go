5b623e6819666c5a680c3e8255d50491019e2773
MaxFuls <maxfuls2000@gmail.com>
2024-12-17 11:47:45 +0300

    delete excessive comments lines

--- internal/handlers/molar_handler.go
+++ internal/handlers/molar_handler.go
@@ -31,37 +31,6 @@ func MolarGetHandler(c echo.Context) error {
 	return c.HTMLBlob(200, content)
 }
 
-// func MolarPostHandler(c echo.Context) error {
-//
-// formula := c.FormValue("formula")
-// parsed := parsing.ParseFormula(formula)
-// elements := make(map[string]float64)
-//
-// total := 0.0
-// for key, count := range parsed {
-// mass, err := parsing.GetMolarMass(key)
-// if err != nil {
-// return c.String(400, "pizdec")
-// }
-// elements[key] = mass * float64(count)
-// total += elements[key]
-// }
-//
-// var disc ElementDiscription
-// disc.Total = fmt.Sprintf("%.3f g/mol", total)
-// disc.Formula = formula
-// for key, count := range parsed {
-// russian, _ := parsing.GetRussianName(key)
-// percent := (elements[key] / total) * 100;
-// disc.Elements = append(disc.Elements,
-// ElementInfo{key, []string{"Название: " + russian,
-//   "Масса в соединении: " + fmt.Sprintf("%.3f g/mol", elements[key]),
-//   "Количество атомов: " + strconv.Itoa(count),
-//   "Процент от общей массы: " + fmt.Sprintf("%.3f %%", percent)}})
-// }
-// return c.Render(http.StatusOK, "molar", disc)
-// }
-
 func MolarPostHandler(c echo.Context) error {
 	config := config.LoadConfig()
 	db, closeFunc, err := database.OpenDB(config.Driver, config.Dns)
